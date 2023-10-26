02b45cb81be026f835181e22a684c77a22c1d8f7
arzumammadov1 <mi7fqqg3l@code.edu.az>
2023-10-27 01:00:07 +0400

    js.task

---
+++ script.js
@@ -0,0 +1,57 @@
+// Task 16
+
+// function findbolen() {
+//     let eded = 20
+//     for (let i = 1; i < eded; i++) {
+//         if (eded % i == 0) {
+//             console.log(i);
+
+//         }
+//     }
+// }
+// findbolen()
+
+
+
+
+
+// Task 17
+
+// function findbolen() {
+//     let eded = 30
+//     let say = 0
+//     for (let i = 1; i < eded; i++) {
+//         if (eded % i == 0) {
+//             say = say + 1
+//         }
+//     }
+//     console.log(say);
+// }
+// findbolen()
+
+
+
+// Task 18
+
+// function findbolen() {
+//     let eded = 15
+//     let cem = 0
+//     for (let i = 1; i < eded; i++) {
+//         if (eded % i == 0) {
+//             cem = cem + i
+//         }
+//     }
+//     console.log(cem);
+// }
+// findbolen()
+
+
+
+// Task 20
+
+
+
+
+
+
+
